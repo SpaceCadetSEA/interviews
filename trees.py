dba580d26fa1ab8da31b37dc638cdbddf1ad0f79
Space <andrewtheis4@gmail.com>
2025-07-23 17:47:23 -0700

    post stream progress

--- trees.py
+++ trees.py
@@ -1,12 +1,13 @@
 from typing import List
 from collections import deque
+from math import inf
 from data_structures.trees import BinaryTree, TreeNode
 
 
 def level_order_traversal(root):
     if not root:
         return "None"
-    
+
     current_queue = deque()
     next_queue = deque()
     result = ""
@@ -39,35 +40,147 @@ def word_ladder(src: str, dest: str, words: List[str]) -> int:
 
     q.append(src)
     while q:
-        curr_word = q.popleft()
-        seen_words = []
-        for word in word_set:
-            if word in seen_words:
-                break
-            # compare words
-            offset = 0
-            char_ptr = 0
+        number_steps += 1
+        size = len(q)
+        for _ in range(size):
+            curr_word = q.popleft()
 
-            while char_ptr < len(curr_word):
-                if curr_word[char_ptr] != word[char_ptr]:
-                    offset += 1
-                char_ptr += 1
-            
-            if offset == 0:
-                return number_steps + 1
-            elif offset == 1:
-                q.append(word)
-                seen_words.append(word)
-        for word in seen_words:
-            word_set.remove(word)
-            number_steps += 1
+            for i in range(len(curr_word)):
+                alpha = "abcdefghijklmnopqrstuvwxyz"
+                for c in alpha:
+                    temp = list(curr_word)
+                    temp[i] = c
+                    temp = "".join(temp)
+
+                    if temp == dest:
+                        return number_steps + 1
+                    if temp in word_set:
+                        q.append(temp)
+                        word_set.remove(temp)
     return number_steps
 
 
-if __name__ == '__main__':
-    values = [100, 50, 200, 25, 75, 300, 10, 350, 15]
+def vertical_order(root):
+    if not root:
+        return []
+
+    q = deque()
+    col_index = 0
+
+    q.append((col_index, root))
+    hash_map = {}
+
+    while q:
+        curr_col_index, curr_node = q.popleft()
+
+        if curr_col_index not in hash_map.keys():
+            hash_map[curr_col_index] = [curr_node.data]
+        else:
+            hash_map[curr_col_index].append(curr_node.data)
+
+        if curr_node.left:
+            q.append((curr_col_index - 1, curr_node.left))
+        if curr_node.right:
+            q.append((curr_col_index + 1, curr_node.right))
+
+    all_indices = list(hash_map.keys())
+    all_indices.sort()
+
+    result = []
+    for idx in all_indices:
+        result.append(hash_map[idx])
+    return result
+
+
+def _serialize(root: TreeNode, res=[]):
+    if not root:
+        res.append(None)
+    else:
+        res.append(root.data)
+        _serialize(root.left, res)
+        _serialize(root.right, res)
+
+
+def serialize(root: TreeNode) -> List[int]:
+    res = []
+    _serialize(root, res)
+    return res
+
+
+def _deserialize(stream: List[int]) -> TreeNode:
+    global idx
+    if not stream:
+        return None
+    next_element = stream[idx]
+    idx += 1
+    if not next_element:
+        return None
+    root = TreeNode(next_element)
+    root.left = _deserialize(stream)
+    root.right = _deserialize(stream)
+    return root
+
+
+def deserialize(stream: List[int]) -> TreeNode:
+    global idx
+    idx = 0
+    return _deserialize(stream)
+
+
+def _max_contrib(root: TreeNode) -> int:
+    global max_sum
+    if not root:
+        return 0
+    left_subtree_max = _max_contrib(root.left)
+    right_subtree_max = _max_contrib(root.right)
+    
+    left_subtree = 0
+    right_subtree = 0
+
+    if left_subtree_max > 0:
+        left_subtree = left_subtree_max
+    if right_subtree_max > 0:
+        right_subtree = right_subtree_max
+    
+    path_max = root.data + left_subtree + right_subtree
+    max_sum = max(max_sum, path_max)
+    return root.data + max(left_subtree, right_subtree)
+
+
+def max_path_sum(root: TreeNode) -> int:
+    global max_sum
+    max_sum = -inf
+    _max_contrib(root)
+    return max_sum
+
+
+def _build_tree_rec(p_order, i_order):
+    global p_idx
+    if not i_order:
+        return None
+    
+    root_val = p_order[p_idx]
+    p_idx += 1
+    root = TreeNode(root_val)
+    i_idx = i_order.index(root.data)
+
+    root.left = _build_tree_rec(p_order, i_order[:i_idx])
+    root.right = _build_tree_rec(p_order, i_order[i_idx + 1:])
+    return root
+
+
+def build_tree(p_order, i_order):
+    global p_idx
+    p_idx = 0
+    return _build_tree_rec(p_order, i_order)
+
+
+if __name__ == "__main__":
+    values = [-8, 2, 17, 1, 4, 19, 5]
     tree_nodes = [TreeNode(val) for val in values]
     tree = BinaryTree(tree_nodes)
     # print(level_order_traversal(tree.root))
 
-    print(word_ladder("zz", "nx", ["nz", "nx"]))
+    # print(serialize(tree.root))
+    # print(max_path_sum(tree.root))
+    print(build_tree([3,9,20,15,7], [9,3,15,20,7]))
