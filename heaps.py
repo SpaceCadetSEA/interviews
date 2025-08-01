d1514675829f3222c54774d741cf6b1fd9f0bec4
Andrew <andrewtheis4@gmail.com>
2025-07-31 19:54:48 -0700

    eod wednesday on a thursday

--- heaps.py
+++ heaps.py
@@ -1,5 +1,6 @@
 from heapq import heappush, heappop, heappushpop, heapreplace
 from collections import Counter
+from math import sqrt
 
 """
 FUNCTION findTopKElements(arr, k):
@@ -42,18 +43,65 @@ def reorganize_string(string):
 
     for key, val in frequency.items():
         heappush(max_heap, (-1 * val, key))
-    
+
     res = ""
 
     while max_heap:
-        pass
+        (next_val, next_char) = heappop(max_heap)
+        if len(res) == 0 or res[-1] != next_char:
+            res += next_char
+            if next_val + 1 < 0:
+                heappush(max_heap, (next_val + 1, next_char))
+        elif res[-1] == next_char:
+            if len(max_heap) > 0:
+                (second_next_val, second_next_char) = heappop(max_heap)
+                res += second_next_char
+                if second_next_val + 1 < 0:
+                    heappush(max_heap, (second_next_val + 1, second_next_char))
+                heappush(max_heap, (next_val, next_char))
+            else:
+                return ""
 
     return res
 
 
+class KthLargest:
+    # Constructor to initialize heap and add values in it
+    def __init__(self, k, nums):
+        self.k = k
+        self.nums = nums
+        self.h = []
+        for num in nums:
+            self.add(num)
+
+    # Adds element in the heap and return the Kth largest
+    def add(self, val):
+        if len(self.h) < self.k:
+            heappush(self.h, val)
+        else:
+            curr_min = heappop(self.h)
+            if val > curr_min:
+                heappush(self.h, val)
+            else:
+                heappush(self.h, curr_min)
+        return self.h[0]
+
+
+def k_closest(points, k):
+    h = []
+    x0, y0 = (0, 0)
+    for x, y in points:
+        distance = sqrt(((x - x0) ** 2 + (y - y0) ** 2)) * -1
+        if len(h) < k:
+            heappush(h, (distance, (x, y)))
+        else:
+            curr_val, (curr_x, curr_y) = heappop(h)
+            if distance > curr_val:
+                heappush(h, (distance, (x, y)))
+            else:
+                heappush(h, (curr_val, (curr_x, curr_y)))
+    return [[x, y] for _, (x, y) in h]
+
+
 if __name__ == "__main__":
-    print(
-        top_k_frequent(
-            [6, 0, 1, 4, 9, 7, -3, 1, -4, -8, 4, -7, -3, 3, 2, -3, 9, 5, -4, 0], 6
-        )
-    )
+    print(reorganize_string("aaabc"))
