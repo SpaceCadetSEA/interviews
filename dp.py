2fad81b764bdceedfc054f45388f44a5dc183efd
Space <andrewtheis4@gmail.com>
2025-08-08 16:41:32 -0700

    EOS: end of stream

--- dp.py
+++ dp.py
@@ -2,10 +2,17 @@ import math
 
 
 def coin_change(coins, total):
+    """
+    time complexity: O(n * m), where n = total and m = num of coins
+    space complexity:
+    """
     if total < 1:
         return 0
-    counter = [math.inf for _ in range(total)]
-    return _coin_change_rec(coins, total, counter)
+    # memoization
+    counter = [math.inf for _ in range(total)]  # O(n)
+    return _coin_change_rec(
+        coins, total, counter
+    )  # also calling recursive function O(n+) times
 
 
 def _coin_change_rec(coins, total, counter):
@@ -17,14 +24,76 @@ def _coin_change_rec(coins, total, counter):
     if not math.isinf(counter[total - 1]):
         return counter[total - 1]
     # otherwise, we haven't seen the result yet
-    minimum = -math.inf
-    for s in coins:
-        result = _coin_change_rec(coins, total - s, counter)
+    minimum = math.inf
+    for s in coins:  # looping over coins O(m)
+        # Min(total) = Min(total − C) + 1
+        result = _coin_change_rec(coins, total - s, counter)  # total decremented O(n)
         if result >= 0 and result < minimum:
             minimum = 1 + result
 
     counter[total - 1] = minimum if not math.isinf(minimum) else -1
     return counter[total - 1]
 
-if __name__ == '__main__':
-    print()
+
+def longest_subsequence_bottom_up(nums):
+    """
+    This is the final version of the solution from educative.io. 
+    It runs in O(n^2)
+    """
+    n = len(nums)
+    cache = [1 for _ in range(n)]
+
+    for curr in range(1, n):
+        for prev in range(curr):
+            if nums[prev] < nums[curr]:
+                cache[curr] = max(cache[curr], cache[prev] + 1)
+
+    return max(cache)
+
+
+def longest_subsequence(nums):
+    """
+    YOU DO NOT USE BS ON THE NUMS SEQUENCE...
+
+    You use BS in order to find the index that the value WOULD have
+    if it were in a sorted list.
+
+    O(n log n)
+    """
+    tails = []
+    for num in nums:
+        pos = _longest_subsequence_bst(num, tails)
+        if pos == len(tails):
+            tails.append(num)
+        else:
+            tails[pos] = num
+    return len(tails)
+
+
+def _longest_subsequence_bst(target, tails):
+    left, right = 0, len(tails)
+    while left < right:
+        mid = (left + right) // 2
+        if tails[mid] < target:
+            left = mid + 1
+        else:
+            right = mid
+    return left
+
+
+def climb_stairs(n):
+    lookup = [0] * n
+
+    for i in range(n - 1, -1, -1):
+        if i == n - 1:
+            lookup[i] = 1
+        elif i == n - 2:
+            lookup[i] = 2
+        else:
+            lookup[i] = lookup[i + 1] + lookup[i + 2]
+
+    return lookup[0]
+
+
+if __name__ == "__main__":
+    print(climb_stairs(5))
